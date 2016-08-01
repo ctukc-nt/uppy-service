4deb2e23b97f8c9f8662c98da03c22898a593172
ctukc <ctukc.nt@gmail.com>
2016-08-01 11:51:06 +0300

    1

--- UPPY.Services.DataManagerService/UppyDataManagerService.svc.cs
+++ UPPY.Services.DataManagerService/UppyDataManagerService.svc.cs
@@ -1,11 +1,19 @@
-﻿using NLog;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using NLog;
 using UPPY.Services.DataManagers;
 
 namespace UPPY.Services.DataManagerService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [LoggingServiceBehavior]
     public partial class UppyDataManagerService
+
     {
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
@@ -15,6 +23,38 @@ namespace UPPY.Services.DataManagerService
         {
             _dataManagers = dataManagers;
             _logger.Trace("Created instance");
+        }
+    }
+
+    class LoggingErrorHandler : IErrorHandler
+    {
+        public void ProvideFault(Exception error, MessageVersion version, ref Message fault)
+        {
+        }
+        public bool HandleError(Exception error)
+        {
+            return false; // здесь можно ставить бряки, логировать и т.п.
+        }
+    }
+    [AttributeUsage(AttributeTargets.Class)]
+    public class LoggingServiceBehaviorAttribute : Attribute, IServiceBehavior
+    {
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+        }
+        public void AddBindingParameters(
+            ServiceDescription serviceDescription,
+            ServiceHostBase serviceHostBase,
+            Collection<ServiceEndpoint> endpoints,
+            BindingParameterCollection bindingParameters)
+        {
+        }
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            foreach (ChannelDispatcher channelDispatcher in serviceHostBase.ChannelDispatchers)
+            {
+                channelDispatcher.ErrorHandlers.Add(new LoggingErrorHandler());
+            }
         }
     }
 }
