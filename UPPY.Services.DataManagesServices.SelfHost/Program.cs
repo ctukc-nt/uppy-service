2a4586acbf57629b71eea29b36fddc2d6f7ca000
ctukc <ctukc.nt@gmail.com>
2016-12-01 10:56:08 +0300

    ядро

--- UPPY.Services.DataManagesServices.SelfHost/Program.cs
+++ UPPY.Services.DataManagesServices.SelfHost/Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
+using NLog;
 
 namespace UPPY.Services.DataManagersServicesSelfHost
 {
     class Program
     {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
             var service = new DataServiceBase();
             try
             {
-                service.Start(new string[] { });
+                service.Start(new string[] {});
                 Console.WriteLine("Press Enter to quit.");
                 Console.ReadLine();
             }
+            catch (Exception ex)
+            {
+                _logger.Error(ex);
+            }
             finally
             {
+               
                 service.Stop();
             }
         }
